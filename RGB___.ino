0b9c304cb686472c64a925b0fd6edd3ca28efa95
UvularGecko2125 <70552300+UvularGecko2125@users.noreply.github.com>
2020-10-06 09:51:09 +0800

    Update RGB___.ino

--- RGB___.ino
+++ RGB___.ino
@@ -3,10 +3,12 @@ int y =5;
 void setup() {
   // put your setup code here, to run once:
   pinMode (9,OUTPUT);
+  pinMode (10,OUTPUT);
 }
 void loop() {
   // put your main code here, to run repeatedly:
    analogWrite(9,x);
+   analogWrite(10,x);
    x = x-y;
    if(x ==0 || x ==255)
    y = -y;
